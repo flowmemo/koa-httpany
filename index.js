95ff4e34a0cf4ca42b32c397112d752baabe8a28
flowmemo <flowmemo@outlook.com>
2016-06-28 23:52:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function anyStatus (ctx, next) {
+  const status = ctx.params.status
+  if (status === 'default') {
+    return next()
+  }
+  try {
+    ctx.status = parseInt(status)
+  } catch (err) {
+    console.log(err)
+    ctx.status = 501
+  }
+  return next()
+}
+
+function anyHeaders (ctx, next) {
+  try {
+    ctx.set(ctx.query)
+  } catch (err) {
+    console.log(err)
+    ctx.status = 501
+  }
+  if (!ctx.get('Access-Control-Allow-Origin')) {
+    ctx.set('Access-Control-Allow-Origin', '*')
+  }
+  return next()
+}
+
+module.exports = {
+  anyStatus: anyStatus,
+  anyHeaders: anyHeaders
+}
